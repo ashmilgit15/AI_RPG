ffec7855e475d4ef8873466bb55ebf947bebafd1
ashmilgit15 <ashmildax15@gmail.com>
2026-06-15 20:16:04 +0530

    inital commit

---
+++ character.py
@@ -0,0 +1,13 @@
+dict = {
+    "name":name,
+    "level":level,
+    "skills":{
+        "python":0,
+        "discipline":0,
+    },
+    "achievements":[],
+
+}
+
+def print_character(char):
+    print(char)
