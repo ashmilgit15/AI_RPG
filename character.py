b4b251bec8d649665221a8aad5af0d447fedd407
ashmilgit15 <ashmildax15@gmail.com>
2026-06-15 21:41:06 +0530

    Added write json logic

--- character.py
+++ character.py
@@ -1,4 +1,9 @@
-dict = {
+import json
+
+name = "Ashmil"
+level = 0
+
+character = {
     "name":name,
     "level":level,
     "skills":{
@@ -9,5 +14,24 @@ dict = {
 
 }
 
+def save_character(char):
+    with open("character.json","w") as file:
+        json.dump(char,file)
+
+def load_character():
+    with open("character.json","r") as file:
+        char_json = json.load(file)
+    return char_json            
+
 def print_character(char):
-    print(char)
+    print(f"Name: {char['name']}")                 
+    print(f"level: {char['level']}")
+    print("Skills:... ")
+    for skill_name,skill_value in char['skills'].items():
+        print(f" {skill_name}:{skill_value}")
+
+
+print_character(character)
+save_character(character)
+loaded = load_character()
+print(loaded)
